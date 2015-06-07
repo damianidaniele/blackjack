717776d291ab99ab220c84a4f2ec42c8dbcc7d01
Daniele Damiani <danieledamiani@yahoo.it>
2015-06-07 15:02:23 +0100

    adding the game page

---
+++ app/scripts/controllers/game.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blackjackApp.controller:GameCtrl
+ * @description
+ * # GameCtrl
+ * Game Controller of the blackjackApp
+ */
+
+angular.module( 'blackjackApp' )
+  .controller( 'GameCtrl', [  '$scope', '$routeParams', 'GameService', function ( $scope, $routeParams, GameService ) {
+   	
+ 
+    $scope.init = function( numberOfPlayers ) { 
+    	var howManyPlayers = numberOfPlayers === !undefined ? numberOfPlayers : $routeParams.players;
+		$scope.GameService = GameService;
+
+    	GameService
+		.init( howManyPlayers )
+		.then( function( data ) {
+			$scope.game = data;
+		});
+	};
+
+	$scope.hit = function( player ) {
+		player.hand.push( $scope.game.deck.shift() );
+
+		if ( player.calculatePoints() ) {
+			$scope.GameService.nextPlayer( player );
+		}
+	};
+
+	$scope.stick = function( player ) {
+		$scope.GameService.nextPlayer( player );
+	};
+
+	$scope.newGame = function() {
+		$scope.init();
+	}
+  }]);
