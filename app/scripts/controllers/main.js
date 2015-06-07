02de0c36fe59e6134f0c055bf77a4468dc19709d
Daniele Damiani <danieledamiani@yahoo.it>
2015-06-07 14:55:16 +0100

    main controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,13 +5,31 @@
  * @name blackjackApp.controller:MainCtrl
  * @description
  * # MainCtrl
- * Controller of the blackjackApp
+ * Main Controller of the blackjackApp
  */
-angular.module('blackjackApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module( 'blackjackApp' )
+  .controller( 'MainCtrl', ['$scope', '$location', function ( $scope, $location ) {
+
+  	$scope.players = 1;
+  	$scope.invalid = false;
+
+  	function validate( value ) {
+  		if ( isNaN( parseInt( value ) ) && !isFinite( value ) ) {
+  			return false;
+  		} 
+		if ( parseInt( value ) < 1 || parseInt( value ) > 7 ) {
+			return false;
+		}
+		return true;
+  	} 
+
+  	$scope.play = function() {
+
+  		if ( validate( $scope.players ) ) {
+  			$location.path( '/play/' + $scope.players );
+  		} else {
+  			$scope.invalid = true;
+  		}
+  	};
+    
+  }]);
